aa416988d2c3f4767333f0c24261735509df191d
Raj Kashikar <65059626+raj200501@users.noreply.github.com>
2024-07-01 00:59:22 -0700

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
